0e78919c065e7bf1941da332f256cee8c093a1a0
Kyle Morel <kyle.1.morel@gov.bc.ca>
2025-02-03 09:48:13 -0800

    Update developer view to include filterable table for yars data

--- app/src/routes/v1/yars.ts
+++ app/src/routes/v1/yars.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 
 import { yarsController } from '../../controllers';
+import { hasAuthorization } from '../../middleware/authorization';
 import { requireSomeAuth } from '../../middleware/requireSomeAuth';
 import { requireSomeGroup } from '../../middleware/requireSomeGroup';
-import { GroupName } from '../../utils/enums/application';
+import { Action, GroupName } from '../../utils/enums/application';
 
 import type { NextFunction, Request, Response } from 'express';
 
 const router = express.Router();
 router.use(requireSomeAuth);
 router.use(requireSomeGroup);
+
+// Not exposing this resource
+const resource = 'YARS';
 
 router.get('/groups', (req: Request, res: Response, next: NextFunction): void => {
   yarsController.getGroups(req, res, next);
@@ -23,6 +27,18 @@ router.delete(
   '/subject/group',
   (req: Request<never, never, { sub: string; group: GroupName }>, res: Response, next: NextFunction): void => {
     yarsController.deleteSubjectGroup(req, res, next);
+  }
+);
+
+router.get('/permissions', (req: Request, res: Response, next: NextFunction): void => {
+  yarsController.getPermissions(req, res, next);
+});
+
+router.get(
+  '/group_role_policy_vw',
+  hasAuthorization(resource, Action.READ),
+  (req: Request, res: Response, next: NextFunction): void => {
+    yarsController.getGroupRolePolicyVw(req, res, next);
   }
 );
 
